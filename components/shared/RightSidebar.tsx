1ca03eb29188af9ad02b82356bf39107eedce8b4
pioalpha <pioalpha@gmail.com>
2024-07-13 13:35:09 -0300

    Implements 018_rightsidebar

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -1,7 +1,81 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import RenderTag from "./RenderTag";
+
+const topQuestions = [
+  {
+    _id: 1,
+    title: "Question Question Question Question Question Question Question1",
+  },
+  {
+    _id: 2,
+    title: "Question Question Question Question Question Question Question 2",
+  },
+  {
+    _id: 3,
+    title: "Question Question Question Question Question Question Question 3",
+  },
+  {
+    _id: 4,
+    title: "Question Question Question Question Question Question Question 4",
+  },
+  {
+    _id: 5,
+    title: "Question Question Question Question Question Question Question 5",
+  },
+];
+
+const popularTags = [
+  { _id: 1, name: "javascript", totalQuestions: 5 },
+  { _id: 2, name: "react", totalQuestions: 5 },
+  { _id: 3, name: "next", totalQuestions: 5 },
+  { _id: 4, name: "vue", totalQuestions: 2 },
+  { _id: 5, name: "redux", totalQuestions: 1 },
+];
 
 const RightSidebar = () => {
-  return <div>RightSidebar</div>;
+  return (
+    <section className="background-light900_dark200 light-border custom-scrollbar sticky right-0 top-0 flex h-screen w-[350px] flex-col overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden">
+      <div>
+        <h3 className="h3-bold text-dark200_light900">Top Questions</h3>
+        <div className="mt-7 flex w-full flex-col gap-[30px]">
+          {topQuestions.map((question) => (
+            <Link
+              href={`/questions/${question._id}`}
+              key={question._id}
+              className="flex cursor-pointer items-center justify-between gap-7"
+            >
+              <p className="body-medium text-dark500_light700">
+                {question.title}
+              </p>
+              <Image
+                src="/assets/icons/chevron-right.svg"
+                alt="chevron right"
+                width={20}
+                height={20}
+                className="invert-colors"
+              />
+            </Link>
+          ))}
+        </div>
+      </div>
+      <div className="mt-16">
+        <h3 className="h3-bold text-dark200_light900">Popular Tags</h3>
+        <div className="mt-7 flex w-full flex-col gap-4">
+          {popularTags.map((tag) => (
+            <RenderTag
+              key={tag._id}
+              _id={tag._id}
+              name={tag.name}
+              totalQuestions={tag.totalQuestions}
+              showCount
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default RightSidebar;
