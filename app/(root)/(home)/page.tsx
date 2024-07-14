ade522ae875ceddc8b784fc6cd54f4454f97ed71
pioalpha <pioalpha@gmail.com>
2024-07-14 00:53:08 -0300

    Implement QuestionCard

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,10 +1,41 @@
+import QuestionCard from "@/components/cards/QuestionCard";
 import HomeFilters from "@/components/home/HomeFilters";
 import Filter from "@/components/shared/Filter";
+import NoResult from "@/components/shared/NoResult";
 import LocalSearch from "@/components/shared/search/LocalSearch";
 import { Button } from "@/components/ui/button";
 import { HomePageFilters } from "@/constants/filters";
 import Link from "next/link";
 import React from "react";
+
+const questions = [
+  {
+    _id: 1,
+    title: "Cascading Deletes in SQLAlchemy?",
+    tags: [
+      { _id: 1, name: "python" },
+      { _id: 2, name: "sql" },
+    ],
+    author: { _id: 1, name: "Jhon Doe", picture: "/assets/icons/avatar.svg" },
+    upvotes: 10,
+    views: 100,
+    answers: [],
+    createdAt: new Date("2021-09-01T12:00:00.000Z"),
+  },
+  {
+    _id: 2,
+    title: "How to center a div?",
+    tags: [
+      { _id: 3, name: "css" },
+      { _id: 4, name: "html" },
+    ],
+    author: { _id: 2, name: "Jane Smith", picture: "/assets/icons/avatar.svg" },
+    upvotes: 10,
+    views: 100,
+    answers: [],
+    createdAt: new Date("2023-09-01T13:00:00.000Z"),
+  },
+];
 
 const Home = () => {
   return (
@@ -32,6 +63,31 @@ const Home = () => {
         />
       </div>
       <HomeFilters />
+
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {questions.length > 0 ? (
+          questions.map((question) => (
+            <QuestionCard
+              key={question._id}
+              _id={question._id}
+              title={question.title}
+              tags={question.tags}
+              author={question.author}
+              upvotes={question.upvotes}
+              views={question.views}
+              answers={question.answers}
+              createdAt={question.createdAt}
+            />
+          ))
+        ) : (
+          <NoResult
+            title="There's no question to show"
+            description="Be the first to break the silence! Ask a Question and kickstart the discussion. Our query could be the next big thing others learn from. Get involved!"
+            link="/ask-question"
+            linkTitle="Ask a Question"
+          />
+        )}
+      </div>
     </>
   );
 };
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,54 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const getTimeStamp = (createdAt: Date): string => {
+  const now = new Date();
+  const diffInMs = now.getTime() - createdAt.getTime();
+  const diffInSec = Math.floor(diffInMs / 1000);
+
+  const minute = 60;
+  const hour = 60 * minute;
+  const day = 24 * hour;
+  const month = 30 * day;
+  const year = 365 * day;
+
+  if (diffInSec < minute) {
+    return `${diffInSec} segundos atrás`;
+  } else if (diffInSec < hour) {
+    const diffInMin = Math.floor(diffInSec / minute);
+    return `${diffInMin} minutos atrás`;
+  } else if (diffInSec < day) {
+    const diffInHrs = Math.floor(diffInSec / hour);
+    return `${diffInHrs} horas atrás`;
+  } else if (diffInSec < month) {
+    const diffInDays = Math.floor(diffInSec / day);
+    return `${diffInDays} dias atrás`;
+  } else if (diffInSec < year) {
+    const diffInMonths = Math.floor(diffInSec / month);
+    return `${diffInMonths} meses atrás`;
+  } else {
+    const diffInYears = Math.floor(diffInSec / year);
+    return `${diffInYears} anos atrás`;
+  }
+};
+
+export const humanReadableNumber = (num: number): string => {
+  const units = ["", "K", "M", "B", "T"];
+  let number = num;
+
+  // Determine the order of magnitude
+  let order = 0;
+  while (number >= 1000 && order < units.length - 1) {
+    number = number / 1000;
+    order++;
+  }
+
+  // Format the number to 2 decimal places
+  const formattedNumber = number.toFixed(2);
+
+  return `${formattedNumber}${units[order]}`;
+};
