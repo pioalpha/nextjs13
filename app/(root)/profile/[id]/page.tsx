589fa4771cdb6f2648597648c04f9bd53093b3b1
pioalpha <pioalpha@gmail.com>
2024-07-24 09:58:13 -0300

    Create Users Stats UI

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -10,6 +10,8 @@ import React from "react";
 import { getMonthYear } from "@/lib/utils";
 import ProfileLink from "@/components/shared/ProfileLink";
 import Stats from "@/components/shared/Stats";
+import QuestionsTab from "@/components/shared/QuestionsTab";
+import AnswersTab from "@/components/shared/AnswersTab";
 
 const Page = async ({ params, searchParams }: URLProps) => {
   const { userId: clerkId } = auth();
@@ -70,7 +72,10 @@ const Page = async ({ params, searchParams }: URLProps) => {
           </SignedIn>
         </div>
       </div>
-      <Stats />
+      <Stats
+      totalQuestions={userInfo.totalQuestions}
+      totalAnswers={userInfo.totalAnswers}
+      />
       <div className="mt-10 flex gap-10">
         <Tabs defaultValue="top-posts" className="flex-1">
           <TabsList className="background-light800_dark400 min-h-[42px] p-1">
@@ -82,10 +87,10 @@ const Page = async ({ params, searchParams }: URLProps) => {
             </TabsTrigger>
           </TabsList>
           <TabsContent value="top-posts" className="">
-            POSTS
+            <QuestionsTab />
           </TabsContent>
           <TabsContent value="answers" className="">
-            ANSWERS
+            <AnswersTab />
           </TabsContent>
         </Tabs>
       </div>
---
+++ components/shared/QuestionsTab.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const QuestionsTab = () => {
+  return (
+    <div>QuestionsTab</div>
+  )
+}
+
+export default QuestionsTab
