60a2229de7b5e7144e85aa4523e49cc338f33bc7
pioalpha <pioalpha@gmail.com>
2024-07-26 15:25:27 -0300

    Manage Filter state

--- app/(root)/question/[id]/page.tsx
+++ app/(root)/question/[id]/page.tsx
@@ -44,11 +44,11 @@ const page = async ({ params, searchParams }) => {
             <Votes
               type="Question"
               itemId={JSON.stringify(result._id)}
-              userId={JSON.stringify(mongoUser._id)}
+              userId={JSON.stringify(mongoUser?._id)}
               upvotes={result.upvotes.length}
-              hasUpVoted={result.upvotes.includes(mongoUser._id)}
+              hasUpVoted={result.upvotes.includes(mongoUser?._id)}
               downvotes={result.downvotes.length}
-              hasDownVoted={result.downvotes.includes(mongoUser._id)}
+              hasDownVoted={result.downvotes.includes(mongoUser?._id)}
               hasSaved={mongoUser?.saved.includes(result._id)}
             />
           </div>
@@ -96,14 +96,14 @@ const page = async ({ params, searchParams }) => {
 
       <AllAnswers
         questionId={result._id}
-        userId={mongoUser._id}
+        userId={mongoUser?._id}
         totalAnswers={result.answers.lenght}
       />
 
       <Answer
         question={result.content}
         questionId={JSON.stringify(result._id)}
-        authorId={JSON.stringify(mongoUser._id)}
+        authorId={JSON.stringify(mongoUser?._id)}
       />
     </>
   );
